107b4e9df50afaa3c00c39f24575d6651c37dcbb
Bryan-cpu255 <69003202+Bryan-cpu255@users.noreply.github.com>
2021-04-22 12:22:18 -0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+function Main(){
+    var num1 = document.getElementById("num1").value;//10
+    var num2 = document.getElementById("num2").value;//15
+    
+    var suma = parseInt(num1) + parseInt(num2);//25
+    
+    alert(suma)
+    console.log(suma)
+}
+
+var btn = document.getElementById("btn");
+btn.addEventListener(´click, Main)
